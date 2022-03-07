6fc56fb28c124d6a70eaf54f098114ac1c4153f5
giedrius <giedriustum@gmail.com>
2022-03-07 04:13:16 +0200

    Implemented Polynomial function.

--- src/Fun/PolynomialFunction.php
+++ src/Fun/PolynomialFunction.php
@@ -4,16 +4,54 @@ namespace Kukulis\Math\Fun;
 
 class PolynomialFunction implements FunctionInterface
 {
+    /** @var Point[] */
     protected array $points = [];
 
     public function calculate(float $x): float
     {
-        // TODO: Implement calculate() method.
-        return $x;
+        list($pointFrom, $pointTo) = $this->findInterval($x);
+
+        $dx = $pointTo->getX() - $pointFrom->getX();
+        $dy = $pointTo->getY() - $pointFrom->getY();
+        if ($dx == 0) {
+            return $pointFrom->getY();
+        }
+
+        $alpha = $dy / $dx;
+
+        $subDx = $x - $pointFrom->getX();
+        $subDy = $subDx * $alpha;
+
+        return $pointFrom->getY() + $subDy;
     }
 
 
     public function addPoint(float $x, float $y)
     {
+        $this->points[] = new Point($x, $y);
+
+        usort($this->points, fn(Point $p1, Point $p2) => $p1->getX() <=> $p2->getX());
+    }
+
+    /**
+     * @return Point[] array which consist of two points.
+     */
+    protected function findInterval(float $x): array
+    {
+        $firstPoint = $this->points[0];
+        if ($x < $firstPoint->getX()) {
+            return [new Point(-PHP_FLOAT_MAX, $firstPoint->getY()), $firstPoint];
+        }
+
+        for ($i = 0; $i < count($this->points) - 1; $i++) {
+            $point = $this->points[$i];
+            $nextPoint = $this->points[$i + 1];
+            if ($x >= $point->getX() && $x < $nextPoint->getX()) {
+                return [$point, $nextPoint];
+            }
+        }
+        $lastPoint = $this->points[count($this->points)];
+
+        return [$lastPoint, new Point(PHP_FLOAT_MAX, $lastPoint->getY())];
     }
 }
--- tests/Unit/PolynomialFunctionTest.php
+++ tests/Unit/PolynomialFunctionTest.php
@@ -2,7 +2,33 @@
 
 namespace Tests\Unit;
 
-class PolynomialFunctionTest
+use Kukulis\Math\Fun\PolynomialFunction;
+use PHPUnit\Framework\TestCase;
+
+class PolynomialFunctionTest extends TestCase
 {
+    public function testFunction()
+    {
+        $function = new PolynomialFunction();
+
+        $function->addPoint(-PHP_FLOAT_MAX, 0);
+        $function->addPoint(0.25, 0);
+        $function->addPoint(1, 0.5);
+        $function->addPoint(4, 1);
+        $function->addPoint(PHP_FLOAT_MAX, 1);
+
+
+        $this->assertEquals(0, $function->calculate(0));
+        $this->assertEquals(0, $function->calculate(0.25));
+        $this->assertGreaterThan(0, $function->calculate(0.5));
+        $this->assertLessThan(0.5, $function->calculate(0.5));
+
+        $this->assertEquals(0.5, $function->calculate(1));
+        $this->assertGreaterThan(0.5, $function->calculate(2));
+        $this->assertLessThan(1, $function->calculate(2));
+
+        $this->assertEquals(1, $function->calculate(4));
+        $this->assertEquals(1, $function->calculate(10));
+    }
 
 }
